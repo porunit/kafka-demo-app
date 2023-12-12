5c0b3e40270903990ccc8f279d83b8b027a9a7c8
Vsevolod <114590209+porunit@users.noreply.github.com>
2023-12-12 21:04:32 +0300

    init

---
+++ src/main/java/com/kafkaexample/config/KafkaConfig.java
@@ -0,0 +1,15 @@
+package com.kafkaexample.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic exampleTopic() {
+        return TopicBuilder.name("porunit")
+                .build();
+    }
+}
