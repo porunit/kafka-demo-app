b0f99efe344b0919125dc738abf815964a7125cc
Vsevolod <114590209+porunit@users.noreply.github.com>
2023-12-13 15:20:58 +0300

    rest

---
+++ src/main/java/com/kafkaexample/MessageController.java
@@ -0,0 +1,25 @@
+package com.kafkaexample;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("api/v1/messages")
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final KafkaTemplate<String, Message> kafkaTemplate;
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest request) {
+        Message message = new Message(request.message(), LocalDateTime.now());
+        kafkaTemplate.send("porunit", message);
+    }
+
+}
